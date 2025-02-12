56087df1715d9071d05397427feb68c08dbc7cf1
Ayça Aksoy <aycaaksoy2000@gmail.com>
2025-02-12 19:51:00 +0300

    first commit

---
+++ src/main/java/com/aa/firstquestion/FirstquestionApplication.java
@@ -0,0 +1,13 @@
+package com.aa.firstquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstquestionApplication.class, args);
+	}
+
+}
